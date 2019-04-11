0216cbac1e59eb9b1c4d8fd9aeff796967a71682
ngeshlew <ngugilewis@gmail.com>
2019-04-11 15:08:48 +0300

    Adding Comments

--- FFA/assets/js/generator.js
+++ FFA/assets/js/generator.js
@@ -4,11 +4,10 @@ function generator() {
 	var second = ["Bring an offline behaviour online", "Make it a rental", "Make it cheaper", "Create a marketplace", "Create original or exclusive content", "Simplify a process", "Make it a service / on demand", "Make it mobile native", "Remove features to launch a simpler product", "Target specific segment", "Create a full stack offering", "Build a community", "Make it peer-to-peer", "Target an emerging market", "Make an intermediary layer", "Remove a middleman", "Apply blockchain", "Democratise something expensive", "Look at startups in other markets"];
 	var third = ["Early exit strategy", "Pay-what-you-want", "Freemium model", "Commission-based model", "Auction model", "Advertisement model", "Affiliate fee", "Get-one-give-one model", "Franchise model", "Subscription model", "Pay-per-use model", "License fees", "Single purchase model", "Pay-as-you-go model", "Razor and blade model", "Crowdfunding", "Open source model", "No frills model"];
 	
-	// generate idea
+	// [Math.floor(Math.random() * first.length)] returns a random integer from Travel to Writing and Editing
 	var idea = "In the " + first[Math.floor(Math.random() * first.length)] + " industry, we " + second[Math.floor(Math.random() * second.length)] + " and use  " + third[Math.floor(Math.random() * third.length)] + " to generate revenue.";
 
-	//alert(name);
-
+	// getElementById returns the element that has the ID attribute with the specified value.
 	// remove name div if existing
 	if (document.getElementById("idea")) {
 		document.getElementById("placeholder").removeChild(document.getElementById("idea"));
