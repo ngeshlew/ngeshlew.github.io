530f2d2269ef8444e698d83fd04d3b0f50e07bda
Lewis <ngugilewis@gmail.com>
2019-09-08 14:08:47 +0300

    Update generator

--- readingbooks/assets/js/generator.js
+++ readingbooks/assets/js/generator.js
@@ -13,10 +13,9 @@ function generator() {
 				"Homo Deus: A History of Tomorrow by Yuval Noah Harari | Philosophy | 450 pages | 13 hours 20 minutes to read", 
 				"The Making of a Manager: What to Do When Everyone Looks to You by Julie Zhuo | Leadership | 288 pages | 6 hours 45 minutes to read", 
 				"Mere Christianity by C.S. Lewis | Christian | 227 pages | 5 hours 14 minutes to read"];
-	var Time = ["Bring an offline behaviour online", "Make it a rental", "Make it cheaper", "Create a marketplace", "Create original or exclusive content", "Simplify a process", "Make it a service / on demand", "Make it mobile native", "Remove features to launch a simpler product", "Target specific segment", "Create a full stack offering", "Build a community", "Make it peer-to-peer", "Target an emerging market", "Make an intermediary layer", "Remove a middleman", "Apply blockchain", "Democratise something expensive", "Look at startups in other markets"];
 	
 	// [Math.floor(Math.random() * first.length)] returns a random integer from Travel to Writing and Editing
-	var book = "The book is " + list[Math.floor(Math.random() * list.length)] + " and duration to read is " + time[Math.floor(Math.random() * time.length)] + " and see you when you finish this one!";
+	var book = "The book is " + list[Math.floor(Math.random() * list.length)] + " and see you when you finish this one!";
 
 	// getElementById returns the element that has the ID attribute with the specified value.
 	// remove name div if existing
